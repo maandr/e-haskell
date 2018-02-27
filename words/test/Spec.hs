55e16ddebef2f5be69545015b464a304cc2f6145
Mats Andresen <mandresen@papego.de>
2018-02-27 17:31:14 +0100

    enhance test suite

--- words/test/Spec.hs
+++ words/test/Spec.hs
@@ -1,8 +1,20 @@
+import Data
 import Lib
 import Test.Hspec
 
 main :: IO ()
 main = hspec $ do
-    describe "Test findWords function" $ do
-        it "Should find all languages contained by the grid" $ do
-            (findWords grid languages) `shouldBe` languages
+    describe "formatGrid" $ do
+        it "Should concatenate every line with a newline" $ do
+            (formatGrid ["abc", "def", "ghi"] `shouldBe` "abc\ndef\nghi\n")
+    
+    describe "findWord" $ do
+        it "Should find words that exist on the Grid" $ do
+            findWord grid "HASKELL" `shouldBe` Just "HASKELL"
+            findWord grid "PHP" `shouldBe` Just "PHP"
+        it "Should not find words that do not exist on the Grid" $ do
+            findWord grid "JAVA" `shouldBe` Nothing
+
+    describe "findWords" $ do
+        it "Should find all the words that exist on the Grid" $ do
+            findWords grid languages `shouldBe` languages
