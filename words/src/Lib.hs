122a2fce41afadec2db23d8c3c47c217b2819f95
Mats Andresen <mandresen@papego.de>
2018-02-27 17:21:53 +0100

    extract data intro seperate module

--- words/src/Lib.hs
+++ words/src/Lib.hs
@@ -1,7 +1,5 @@
 module Lib
-    ( grid
-    , languages
-    , getPossibilities
+    ( getPossibilities
     , skew
     , findWord
     , findWords
@@ -39,7 +37,7 @@ skew (l:ls) = l : skew(map indent ls)
     where indent line = '_' : line
 
 findWord :: Grid -> String -> Maybe String
-findWord gird word =
+findWord grid word =
     let possibilities = getPossibilities grid
         found = or (map (findWordInLine word) possibilities)
     in if found then Just word else Nothing
@@ -52,29 +50,3 @@ findWords grid words =
 findWordInLine :: String -> String -> Bool
 findWordInLine word line = word `isInfixOf` line
 --findWordInLine = isInfixOf
-
-grid :: Grid
-grid = [  "__C________R___"
-        , "__SI________U__"
-        , "__HASKELL____B_"
-        , "__A__A_____S__Y"
-        , "__R___B___C____"
-        , "__PHP____H_____"
-        , "____S_LREP_____"
-        , "____I__M_Y__L__"
-        , "____L_E__T_O___"
-        , "_________HB____"
-        , "_________O_____"
-        , "________CN_____"
-        ]
-
-languages = [ "BASIC"
-            , "COBOL"
-            , "CSHARP"
-            , "HASKELL"
-            , "LISP"
-            , "PERL"
-            , "PHP"
-            , "PYTHON"
-            , "RUBY"
-            , "SCHEME"]
