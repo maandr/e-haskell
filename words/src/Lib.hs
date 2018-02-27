3a6612548e68c498476a6c224c3cf04ebb4af1d4
Mats Andresen <mandresen@papego.de>
2018-02-27 14:52:02 +0100

    render word grid

--- words/src/Lib.hs
+++ words/src/Lib.hs
@@ -1,9 +1,36 @@
 module Lib
-    ( someFunc
-    , someString ) where
+    ( grid
+    , languages
+    , formatGrid
+    , renderGrid ) where
 
-someFunc :: IO ()
-someFunc = putStrLn someString
+renderGrid :: IO ()
+renderGrid = putStrLn (formatGrid grid)
 
-someString :: String
-someString = "someString"
+formatGrid :: [String] -> String
+formatGrid = unlines
+
+grid = [  "__C________R___"
+        , "__SI________U__"
+        , "__HASKELL____B_"
+        , "__A__A_____S__Y"
+        , "__R___B___C____"
+        , "__PHP____H_____"
+        , "____S_LREP_____"
+        , "____I__M_Y__L__"
+        , "____L_E__T_O___"
+        , "_________HB____"
+        , "_________O_____"
+        , "________CN_____"
+        ]
+
+languages = [ "BASIC"
+            , "COBOL"
+            , "CSHARP"
+            , "HASKELL"
+            , "LISP"
+            , "PERL"
+            , "PHP"
+            , "PYTHON"
+            , "RUBY"
+            , "SCHEME"]
