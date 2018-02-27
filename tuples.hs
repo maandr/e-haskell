e515f0ca4069f5e91d7f830e60fae55b95032841
Mats Andresen <mandresen@papego.de>
2018-02-27 12:21:47 +0100

    basic tuples

---
+++ tuples.hs
@@ -0,0 +1,13 @@
+module Tuples where
+
+-- tuple containing two integers
+intTuple :: (Int, Int)
+intTuple = (10, 100)
+
+-- tuple containing two booleans
+boolTuple :: (Bool, Bool)
+boolTuple = (True, False)
+
+-- tuple of string and integer
+stringIntTuple :: (String, Int)
+stringIntTuple = ("john", 35)
