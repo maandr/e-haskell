e9488da5e91126a1572a00e57578806c25f48e03
Mats Andresen <mandresen@papego.de>
2018-02-28 11:33:07 +0100

    more on lists

--- basics/lists.hs
+++ basics/lists.hs
@@ -1,5 +1,7 @@
 module Lists where
 
+import Control.Monad
+
 -- strings are lists of chars
 charList :: [Char]
 charList = ['H', 'e', 'l', 'l', 'o']
@@ -25,3 +27,25 @@ firstElementOfInts = head [1, 2, 3, 4, 5]
 
 -- tail chops of the head of a list and returns the remainder of the list
 lastElementOfInts = tail [1, 2, 3, 4, 5]
+
+-- list ranges
+n :: [Int]
+n = [0..9]
+
+-- infinite lists
+take10 :: [Int]
+take10 = take 10 [0..]
+
+isEvan :: Int -> Bool
+isEvan x = x `mod` 2 == 0
+
+-- list monad notation
+filtered = do
+    i <- [0..10]
+    guard (isEvan i) -- predicate
+    return i
+
+-- list comprehention
+nums = [i | i <- [0..100], isEvan(i)]
+
+evanNumbers n = [i | i <- [0..n], isEvan(i)]
