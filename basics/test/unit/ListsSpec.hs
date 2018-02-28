2b5116804690539876927bd6deffa4ad57142962
Mats Andresen <mandresen@papego.de>
2018-02-28 14:09:32 +0100

    enhance lists test

--- basics/test/unit/ListsSpec.hs
+++ basics/test/unit/ListsSpec.hs
@@ -41,12 +41,31 @@ spec = do
             tail [1, 2, 3] `shouldBe` [2, 3]
         it "Should return the tail of a string" $ do
             tail "Hello" `shouldBe` "ello"
+    
+    describe "take" $ do
+        it "Should take given amount of literals from a list" $ do
+            take 1 [1, 2, 3] `shouldBe` [1]
+            take 2 [1, 2, 3] `shouldBe` [1, 2]
+            take 3 "Hello" `shouldBe` ['H', 'e', 'l']
+        it "Should take given amount of literals from a infinit list" $ do
+            take 3 [2, 4..] `shouldBe` [2, 4, 6]
+        it "Should return empty list when applied to empty list" $ do
+            (take 1 [] :: [Int]) `shouldBe` []
+    
+    describe "repeat" $ do
+        it "Should take a literal and repeat it into an infinit list" $ do
+            (take 5 $ repeat 5) `shouldBe` [5, 5, 5, 5 ,5]
+            (take 3 $ repeat "na") `shouldBe` ["na", "na", "na"]
 
     describe "concat" $ do
         it "Should concatinate a list of lists" $ do
             concat [[1, 2, 3], [4, 5]] `shouldBe` [1, 2, 3, 4, 5]
         it "Should concatinate a list of list including and empty list" $ do
             concat [[1, 2, 3], []] `shouldBe` [1, 2, 3]
+        it "Should concatinate a list of strings" $ do
+            concat ["na", "na", "na"] `shouldBe` "nanana"
+        it "Should concatinate two empty lists" $ do
+            concat [[] :: [Int], [] :: [Int]] `shouldBe` []
 
     describe "isEvan" $ do
         it "Should return True given an evan number" $ do
