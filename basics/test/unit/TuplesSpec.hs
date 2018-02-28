ee5ba6d28d7a6f1102feb4175ba4ece70f8aa949
Mats Andresen <mandresen@papego.de>
2018-02-28 13:42:55 +0100

    enhance tuples tests

--- basics/test/unit/TuplesSpec.hs
+++ basics/test/unit/TuplesSpec.hs
@@ -6,6 +6,16 @@ import Data.Tuple --swap
 spec :: Spec
 spec = do
 
+    describe "Comparison" $ do
+        it "Should compare equality on behalf of the tuples first component, then on second component" $ do
+            (1, 5) > (2, 1) `shouldBe` False
+            (2, 0) > (1, 8) `shouldBe` True
+            (2, 0) >= (2, 5) `shouldBe` False
+            (2, 0) == (2, 5) `shouldBe` False
+            (1, 5) == (1, 5) `shouldBe` True
+            (2, 0) < (2, 5) `shouldBe` True
+            (2, 0) <= (2, 0) `shouldBe` True
+
     describe "fst" $ do
         it "Should return first component of a tuple" $ do
             fst (9, 4) `shouldBe` 9
