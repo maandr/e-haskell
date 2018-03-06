3e13766e8b5330462e27e0608ae118bad899269c
Mats Andresen <mandresen@papego.de>
2018-03-06 16:31:09 +0100

    cleanup aeson basics

--- json/test/unit/AesonSpec.hs
+++ json/test/unit/AesonSpec.hs
@@ -2,16 +2,14 @@
 
 module AesonSpec where
 
-import qualified Data.Text as T
-import qualified Data.Text.Lazy.IO as T
-import qualified Data.Text.Lazy.Encoding as T
-import qualified Data.HashMap.Strict as HashMap
-import qualified Data.Vector as Vector
 import Data.Text
 import Data.Aeson
-import Data.Aeson.Types
+import Data.Aeson.Types ( 
+         object
+        , Value(..)
+    )
 import Test.Hspec
-import GHC.Exts
+import GHC.Exts ( fromList )
 
 spec :: Spec
 spec = do
@@ -22,49 +20,18 @@ spec = do
     let bools = [True, False, False] :: [Bool]
     let strings = ["green", "blue", "red"] :: [String]
 
-    -- with GHC.Exts fromList
     let book :: Value
         book = Object $ fromList [
                 ("pages", Number 99),
                 ("author", String "Franz Kafka")
             ]
 
-    -- with object function
     let movie :: Value
         movie = object [
                 "title" .= String "Star Wars",
                 "genres" .= [String "sci-fi", String "adventure"],
                 "year" .= Number 1977
             ]
-    --{--
-    let parseTuple :: Value -> Parser (String, Bool)
-        parseTuple (Object o) = do
-            -- lookup the "a" field
-            let mbFieldA = HashMap.lookup "a" o
-
-            -- fail if it wasn't found
-            fieldA <- case mbFieldA of
-                Just x -> return x
-                Nothing -> fail "no field 'a'"
-            
-            -- extract the value from it, or fail if it's of the wrong type
-            a <- case fieldA of
-                String x -> return (T.unpack x)
-                _ -> fail "expected a string"
-                
-            -- do all the same for b (in a slightly terser way)
-            b <- case HashMap.lookup "b" o of
-                Just (Bool x) -> return x
-                Just _ -> fail "expected a boolean"
-                Nothing -> fail "no field 'b'"
-
-            return (a, b)
-        parseTuple _ = fail "expected an object"
-    --}
-
-    let parseArray :: Value -> Parser [(String, Bool)]
-        parseArray (Array a) = mapM parseTuple (Vector.toList a)
-        parseArray _ = fail "expected an array"
 
     describe "encode" $ do
         it "should encode literals to json" $ do
@@ -90,5 +57,3 @@ spec = do
         it "should decode json to a list of literals" $ do
             (decode "[1,2,3]" :: Maybe [Int]) `shouldBe` Just [1, 2, 3]
             (decode "maleformed" :: Maybe [Int]) `shouldBe` Nothing
-        --it "should decode json to complex types" $ do
-            --(parseMaybe parseTuple "{\"a\":\"foo\",\"b\":true}" :: Maybe (String, Bool)) `shouldBe` Just ("foo", True)
