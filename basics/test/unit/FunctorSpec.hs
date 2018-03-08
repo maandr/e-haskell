e1f3397d95e080e4902f8c55482f053ffd9ba5f3
Mats Andresen <mandresen@papego.de>
2018-03-08 10:19:24 +0100

    cleanup optional exercise

--- basics/test/unit/FunctorSpec.hs
+++ basics/test/unit/FunctorSpec.hs
@@ -25,22 +25,25 @@ instance IsEmpty (Optional a) where
     isEmpty Null = True
     isEmpty _ = False
 
+empty :: Optional a
+empty = Null
+
 spec :: Spec
 spec = do
 
-    describe "Optional Functor" $ do
-        it "should apply a function using fmap on the Optional functor" $ do
-            -- given
-            let optional = Optional 10 :: Optional Int
-
-            -- when + then
-            (fmap (\x -> x * x) optional) `shouldBe` (Optional 100)
-            isEmpty optional `shouldBe` False
-        it "should apply a function using fmap on a empty Optional functor" $ do
-            -- given
-            let optional = Null :: Optional Int
-
-            (fmap (\x -> x * x) optional) `shouldBe` Null
-            isEmpty optional `shouldBe` True
+    describe "fmap" $ do
+        it "should apply a function on value helt in a optional" $ do
+            fmap (\x -> x * x) (Optional 10) `shouldBe` (Optional 100)
+            fmap (\x -> x ++ x) (Optional "foo") `shouldBe` (Optional "foofoo")
+            fmap (\x -> x ++ x) (Optional [1, 2]) `shouldBe` (Optional [1, 2, 1, 2])
+        it "should return a empty optional when apply a function on a empty optional" $ do
+            fmap (\x -> x * x) Null `shouldBe` Null
+    describe "isEmpty" $ do
+        it "should return False for a optional holding a value" $ do
+            isEmpty (Optional 10) `shouldBe` False
+            isEmpty (Optional "John") `shouldBe` False
+            isEmpty (Optional [1.5, 3.3, 4.3]) `shouldBe` False
+        it "should return True for a empty optional" $ do
+            isEmpty Null `shouldBe` True
 
 
